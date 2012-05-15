72e785777f831ee8d6e04280c6d8cdab4828208b
Dennis <dsb9938@BuildBox.(none)>
2012-05-15 00:27:02 -0400

    Add Smartass2 Governor.

--- GT-P3113_Kernel/include/generated/compile.h
+++ GT-P3113_Kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 4 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Fri May 11 12:59:30 EDT 2012"
+#define UTS_VERSION "#4 SMP PREEMPT Tue May 15 00:17:18 EDT 2012"
 #define LINUX_COMPILE_BY "dsb9938"
 #define LINUX_COMPILE_HOST "BuildBox"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
