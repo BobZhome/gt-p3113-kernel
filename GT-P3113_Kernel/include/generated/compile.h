55ea2abbc50db5e268985971cea3e611a99b145b
bobzhome <bobzhome@cheerful.ful>
2012-06-23 13:34:42 -0400

    rename folder

--- GT-P3113_Kernel/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 4 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 SMP PREEMPT Tue May 15 00:17:18 EDT 2012"
-#define LINUX_COMPILE_BY "dsb9938"
-#define LINUX_COMPILE_HOST "BuildBox"
-#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
